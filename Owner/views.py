2a150fed5974edfe55b38a935727c9c047ae70ac
Kalyan Reddy Yeravati <kalyanyeravati5120@gmail.com>
2023-12-12 09:39:38 +0530

    new commit

--- Owner/views.py
+++ Owner/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from Owner.models import Owner_registration_model
+import re
 
 
 
