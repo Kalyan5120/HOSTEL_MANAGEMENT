26f5e4d0e16b165732d8acc2a29d78ae0cf2f844
Kalyan Reddy Yeravati <kalyanyeravati5120@gmail.com>
2023-12-18 23:18:32 +0530

    commit

--- Customer/urls.py
+++ Customer/urls.py
@@ -13,5 +13,9 @@ urlpatterns=[
     path(route='forgetpswrd/',view=forgetpassword_view,name='forgetpswrd'),
     path(route='customer_otp/<int:pk>/',view=otp_confirm_view,name='customer_otp'),
     path(route='changepswrd/<int:pk>/',view=changepswrd_view,name='changepswrd'),
+    path(route='customer_main/',view=customer_main_view,name='c_main'),
+    path(route='hostel_detail/<int:pk>',view=hostel_detail_view,name='hostel_detail'),
+    path(route='booking/<hostel>/<room>/<bed>/<data1>/<data2>/<data3>/',view=room_book_view,name='booking'),
+    path(route='approved_book/<room>/<bed>/<pk>/',view=approved_room_book_view,name='approved_book'),
     
 ]
