a0e67c3f38a78973f43dcb574312c46fa2d7cbd9
prashanth3127 <prashanthkumara57@gmail.com>
2023-12-19 18:44:54 +0530

    commit

--- Customer/urls.py
+++ Customer/urls.py
@@ -3,17 +3,8 @@ from Customer.views import *
 app_name='customer'
 
 urlpatterns=[
-    path(route='customer_main/',view=customer_main_view,name='c_main'),
-    path(route='customer_register/',view=customer_registration_view,name='c_register'),
-    path(route='customer_login/',view=customer_view,name='customer_login'),
-    path(route='logout/',view=logout_view,name='logout'),
+   
 
-    path(route='list/',view=customer_list,name='list'),
-    path(route='p_details/',view=p_details_view,name='p_details'),
-    path(route='forgetpswrd/',view=forgetpassword_view,name='forgetpswrd'),
-    path(route='customer_otp/<int:pk>/',view=otp_confirm_view,name='customer_otp'),
-    path(route='changepswrd/<int:pk>/',view=changepswrd_view,name='changepswrd'),
-    path(route='customer_main/',view=customer_main_view,name='c_main'),
     path(route='hostel_detail/<int:pk>',view=hostel_detail_view,name='hostel_detail'),
     path(route='booking/<hostel>/<room>/<bed>/<data1>/<data2>/<data3>/',view=room_book_view,name='booking'),
     path(route='approved_book/<room>/<bed>/<pk>/',view=approved_room_book_view,name='approved_book'),
--- Customer/views.py
+++ Customer/views.py
@@ -14,51 +14,9 @@ from Owner.models import hostel_details_model,rooms_details_model,bed_details_mo
 
 
 # Create your views here.
-def customer_registration_view(request):
-    form=cust_form()
-    if request.method=='POST' and request.FILES:
-        form=cust_form(request.POST,request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('/Customer/customer_login')
-    return render(request=request,template_name='customer_register.html',context={'form':form})
 
 
 
-def customer_view(request):
-    form=customer_login()
-    if request.method=='POST':
-        form=customer_login(request.POST)
-        if form.is_valid():
-            user=authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password'])
-        if user:
-            login(request,user)
-            return redirect('/Customer/home')
-        else:
-            messages.error(request,'username or password is incorrect')
-            return redirect('/Customer/customer_login')
-    return render(request=request,template_name='customer_login.html',context={'form':form})
-
-@login_required(login_url='/Customer/customer_login')
-def customer_list(request):
-    form=customer_register.objects.all()
-    return render(request=request,template_name='customer_list.html',context={'form':form})
-   
-
-def p_details_view(request):   
-    return render(request=request,template_name='p_details.html')
-
-@login_required(login_url='/Customer/customer_login')
-def logout_view(request):
-    logout(request)
-    return redirect('/Customer/customer_login')
-    
-
-
-
-
-
-@login_required(login_url='/Customer/customer_login')
 def hostel_detail_view(request,pk):
     res=hostel_details_model.objects.get(hostel_id=pk)
     room=rooms_details_model.objects.filter(hostel_id=pk)
@@ -66,59 +24,8 @@ def hostel_detail_view(request,pk):
     images=gallery_model.objects.all()
     print(res)
     return render(request=request,template_name='hostel_detail.html',context={'hostel':res,'room_details':room,'bed_details':bed,'images':images})
-#forget password 
-
-otp_confirm=None
 
 
-def forgetpassword_view(request):
-    res=customer_register.objects.all().values_list('email')    
-    global otp_confirm
-    if request.method=='POST':
-        otp=random.randint(0000,9999)
-        otp_confirm=otp
-        email=request.POST['email']
-        if (email,) in res:
-            subject='confirm the OTP'
-            msg=f'''hello ,
-                please confirm the otp:{otp}
-                thank you.'''
-            send_mail(subject=subject,message=msg,from_email=settings.EMAIL_HOST_USER,recipient_list=[email,])
-            email_id=customer_register.objects.get(email=email)
-            return redirect(f'/Customer/customer_otp/{email_id.id}/')
-        else:
-            messages.error(request,'email is incorrect')
-    return render(request=request,template_name='forget_password.html')
-
-
-def otp_confirm_view(request,pk):
-    if request.method=='POST':
-        if str(otp_confirm)==str(request.POST['otp_confirm']):
-            return redirect(f'/Customer/changepswrd/{pk}/')
-        else:
-            return redirect('/Customer/forgetpswrd')
-    return render(request=request,template_name='enterotp.html')
-    
-
-def changepswrd_view(request,pk):
-    form=changepswrd_form()
-    if request.method=='POST':
-        res=customer_register.objects.get(id=pk)
-        form=changepswrd_form(request.POST)
-        print(form)
-        if form.is_valid():
-            print("bye")
-            if form.cleaned_data['enter_new_password']==form.cleaned_data['reenter_new_password']:
-                customer_register.objects.filter(id=pk).update(password=make_password(form.cleaned_data['enter_new_password']))
-                return redirect('/Customer/customer_login')
-    return render(request=request,template_name='changepswrd.html',context={'form':form})
-
-
-def customer_main_view(request):
-    return render(request=request,template_name='customer_main.html')
-
-def easy_stay_view(request):
-    return render(request=request,template_name='easy_stay.html')
 
 
 
@@ -161,6 +68,8 @@ def room_book_view(request,hostel,room,bed,data1,data2,data3):
                 messages.success("To sent the email approval from owner")
                 return redirect('/Customer/home/')
     return render(request=request,template_name='book_room.html',context={'form':form})
+
+
 
 def approved_room_book_view(request,room,bed,pk):
     res=bed_details_model.objects.get(bed_id=bed,room_no=room)
